b7c91f80be0e026d78e0fda11da3e069de2ec9d3
Bharath.Bheemireddy <bharath.bheemireddy@redlion.onmicrosoft.com>
2024-11-22 23:10:07 +0530

    bug fixed

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -40,10 +40,8 @@ defaultRoutes.forEach((route) => {
 });
 
 /* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
+devRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
 
 module.exports = router;
